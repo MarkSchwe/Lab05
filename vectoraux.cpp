184c75f030039ea6dcd867c2310942fbdafb5173
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-18 19:56:41 -0400

    Create vectoraux.cpp

---
+++ vectoraux.cpp
@@ -0,0 +1,68 @@
+/*
+  vectorAux.cpp
+  Implementations of template functions 
+  Cosc-220   Lab 7
+
+  Thomas Anastasio
+  Created: February 25, 2003
+  Current: March 13, 2003
+*/
+
+
+//////////   THIS CODE IS PROVIDED TO STUDENTS   /////////////
+
+
+#include "vectoraux.h"
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+void removeDup(std::vector<T> & v)
+{
+//Use Iterator to set pointer to certain spots and use them to delete duplicates.
+std::vector<T>:: iterator y;
+std::vector<T>:: iterator j;
+
+for(y = v.begin(); y < v.end(); y++){
+  for(j = y+1; j < v.end(); j++){
+    if(y == j){
+      v.erase(j);
+    }
+  }
+}
+
+}
+
+
+
+
+template <typename T>
+unsigned seqVectSearch(const std::vector<T> & v, unsigned first, 
+		       unsigned last, const T& target)
+{
+  ///  Complete the code.   Use the text's sequential search
+  ///  algorithm for arrays as a model.
+unsigned tar = 0;
+
+for(unsigned i = first; i <= last; i++){
+tar = v[i];
+if (tar == target){
+  return tar;
+}
+
+}
+  return last;           // return last if target not found
+}
+
+template <typename T>
+void writeVector(const std::vector<T> & v)
+{
+  unsigned i;
+  unsigned n = v.size();
+
+  for (i = 0; i < n; i++)
+    std::cout << v[i] << ' ';
+  std::cout << std::endl;
+}
